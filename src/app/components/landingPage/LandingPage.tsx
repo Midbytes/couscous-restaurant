41bccee6cc558eba63552f75a3c812a4aca24baa
selim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-20 19:58:11 +0100

    feat: Create and style landingPage

---
+++ src/app/components/landingPage/LandingPage.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./landingPage.module.scss";
+import { NEWS_SECTION_ITEM } from "@/app/constant/sectionItem";
+import Section from "../section/Section";
+import Image from "next/image";
+
+function LandingPage() {
+  return (
+    <article className={styles.container}>
+      <h1 className={styles.img}>
+        <Image
+          src={"/assets/landingPicture.svg"}
+          alt="landing-picture"
+          width={1440}
+          height={548}
+        />
+      </h1>
+      <div className={styles.text}>
+        {" "}
+        {NEWS_SECTION_ITEM.map((item) => (
+          <Section {...item} key={item.title} />
+        ))}
+      </div>
+    </article>
+  );
+}
+
+export default LandingPage;
