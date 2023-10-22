6c2ecde32abff4388379841900526ef17794e505
alibeji98 <alibeji98@gmail.com>
2023-10-22 20:20:55 +0200

    test: hook on production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import LandingPage from "./components/landingPage/LandingPage";
 import Navbar from "./components/navBar/Navbar";
+import { useGetFoodsQuery } from "./getFoods.rq.generated";
 import styles from "./page.module.scss";
 
 export default function Home() {
+  const { data } = useGetFoodsQuery();
+  console.log(data);
   return (
     <main className={styles.main}>
       <Navbar />
