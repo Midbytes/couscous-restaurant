ffac70dc690573353d21855bce207fd53e1d8963
alibeji98 <alibeji98@gmail.com>
2023-10-22 14:53:27 +0200

    build: add codegen config

--- codegen.ts
+++ codegen.ts
@@ -3,9 +3,9 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   schema: [
     {
-      "https://couscous.up.railway.app/graphql": {
+      [process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_ENDPOINT || ""]: {
         headers: {
-          authorization: `Bearer 99868a8183c6c85414164e0e714e74ad276a87497f314e5dee129b7f6cd9bc0438c477fb2cffc38150f48fb39ca55b2557a9a0481a53f5211272aded8924596383d7babb1956da7656c329ff0ee5a7aafd727c05e8c3a03e7b4c400f17086be3b16f656bebc04a34e7e0083618616899557273a1496c7925ab67043943f0519b`,
+          authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_BEARER_TOKEN}`,
         },
       },
     },
@@ -16,7 +16,7 @@ const config: CodegenConfig = {
       documents: ["!src/**/*.rq.graphql"],
       plugins: ["typescript"],
     },
-    "./src/": {
+    "src/": {
       documents: ["src/**/*.rq.graphql"],
       preset: "near-operation-file",
       presetConfig: {
@@ -24,7 +24,8 @@ const config: CodegenConfig = {
         baseTypesPath: "generated/graphql.ts",
       },
       config: {
-        fetcher: "@/utils/fetcher#reactQueryFetcher",
+        fetcher: "@/utils/fetcher#fetcher",
+        isReactHook: false,
         exposeQueryKeys: true,
       },
       plugins: ["typescript-operations", "typescript-react-query"],
