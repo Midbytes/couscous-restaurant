dcce4f06c985a9b23c91e0159c97ebedec49cc4d
alibeji98 <alibeji98@gmail.com>
2023-10-27 05:27:23 +0200

    refactor: menu

--- src/app/components/menu/Menu.tsx
+++ src/app/components/menu/Menu.tsx
@@ -4,10 +4,24 @@ import { GetFoodsQuery, useGetFoodsQuery } from "./getFoods.rq.generated";
 import styles from "./menu.module.scss";
 import { Enum_Food_Course } from "@/generated/graphql";
 import { Unpacked } from "@/app/type/utils";
+import { sortByOrder } from "@/app/utils/sortByOrder";
+import { fixTitleFormat } from "@/app/utils/fixTitleFormat";
+import { formatPrice } from "@/app/utils/formatPrice";
 
 type Food = NonNullable<
   Unpacked<Unpacked<NonNullable<GetFoodsQuery["foods"]>>["data"]>["attributes"]
->;
+> & {
+  id: string;
+};
+
+// Order of the sections
+const order = [
+  Enum_Food_Course.Starter,
+  Enum_Food_Course.Main,
+  Enum_Food_Course.Dessert,
+  Enum_Food_Course.Alcohol,
+  Enum_Food_Course.SoftDrink,
+];
 
 export default function Menu() {
   const { data } = useGetFoodsQuery();
@@ -17,33 +31,25 @@ export default function Menu() {
   const courses = useMemo(
     () =>
       foods?.data.reduce<Record<Enum_Food_Course, Food[]>>(
-        (courses, { attributes: food }) => {
+        (courses, { attributes, id }) => {
           // Prevent adding null/undefined to array
-          if (!food) return courses;
+          if (!attributes) return courses;
 
-          let { course } = food;
-
+          const { course } = attributes;
           return {
             ...courses,
-            [course]: [...(courses[course] || []), food],
+            [course]: [...(courses[course] || []), { id, ...attributes }],
           };
         },
         {} as Record<Enum_Food_Course, Food[]>
       ),
     [foods?.data]
   );
-  // Order of the sections
-  const order = ["starter", "main", "dessert", "alcohol", "soft_drink"];
   // Unordered sections retrieved from graphQl request
   const sections = Object.keys(courses ?? []) as Array<Enum_Food_Course>;
   /* Reordering graphQL request sections, new sections that are not inside 
   the order array are placed at the end of the orderedSections array */
-  const orderedSections = sections
-    .sort((a, b) => {
-      return order.indexOf(a) - order.indexOf(b);
-    })
-    .filter((section) => order.indexOf(section) > -1)
-    .concat(sections.filter((section) => order.indexOf(section) == -1));
+  const orderedSections = sortByOrder<Enum_Food_Course>(sections, order);
 
   return (
     <section className="container">
@@ -52,31 +58,25 @@ export default function Menu() {
         ? orderedSections.map((courseItem) => {
             return (
               <ul className={styles.foodList} key={courseItem}>
-                <h3 className={styles.course}>
-                  {
-                    // Fix course formatting and make plural
-                    courseItem.replace(/_/, " ") + "s"
-                  }
-                </h3>
+                <h3 className={styles.course}>{fixTitleFormat(courseItem)}</h3>
                 {
                   // Sort foods by index: low index is higher on the list
                   courses[courseItem]
                     .sort((a, b) =>
                       a.index && b.index ? (a.index >= b.index ? 1 : -1) : 0
                     )
-                    .map((food) => {
+                    .map(({ name, id, price, description }) => {
                       return (
-                        <li
-                          key={courses[courseItem].indexOf(food)}
-                          className={styles.foodListItem}
-                        >
+                        <li key={id} className={styles.foodListItem}>
                           <span className={styles.foodListItemLeft}>
-                            <span className={styles.name}>{food.name}</span>
+                            <span className={styles.name}>{name}</span>
                             <span className={styles.description}>
-                              {food.description}
+                              {description}
                             </span>
                           </span>
-                          <span className={styles.price}>{food.price},-</span>
+                          <span className={styles.price}>
+                            {formatPrice(price)}
+                          </span>
                         </li>
                       );
                     })
