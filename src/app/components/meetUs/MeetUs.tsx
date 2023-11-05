a1158a9d08be8a46c02bff0ca1b7b763f4c4a97f
alibeji98 <alibeji98@gmail.com>
2023-11-05 06:22:20 +0100

    feat: add MeetUs props from strapi request

--- src/app/components/meetUs/MeetUs.tsx
+++ src/app/components/meetUs/MeetUs.tsx
@@ -1,18 +1,21 @@
+"use client";
 import React from "react";
 import styles from "./meetUs.module.scss";
 import Image from "next/image";
-import { MEET_US_SECTION_ITEM } from "@/app/constant/sectionItem";
 import Section from "../section/Section";
+import { useGetAboutUsQuery } from "./getAboutUs.rq.generated";
 
 function MeetUs() {
+  const { data } = useGetAboutUsQuery();
+  const res = data?.aboutUs?.data?.attributes;
   return (
     <article className={`${styles.container} container-row`} id="#meet">
-      <div className={styles.text}>
-        {" "}
-        {MEET_US_SECTION_ITEM.map((item) => (
-          <Section {...item} key={item.title} />
-        ))}
-      </div>
+      {res?.aboutUsDescription && res.Title ? (
+        <Section title={res.Title} description={res.aboutUsDescription} />
+      ) : (
+        //TODO: Add loader
+        ""
+      )}
       <Image
         className={styles.meetUsImage}
         src={"/assets/couscous_illustration.webp"}
