93cfe0d8d195e3bc547fcc5be0b0ecf40a52f1b6
alibeji98 <alibeji98@gmail.com>
2023-11-08 11:34:19 +0100

    fix: use client in reservation component instead of main

--- src/app/components/reservation/Reservation.tsx
+++ src/app/components/reservation/Reservation.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-"use client";
 import "glider-js/glider.min.css";
-import LandingPage from "./components/landingPage/LandingPage";
+import News from "./components/news";
 import Delicacies from "./components/delicacies/Delicacies";
-import MeetUs from "./components/meetUs/MeetUs";
 import Menu from "./components/menu/Menu";
 import Navbar from "./components/navBar/Navbar";
 import Reservation from "./components/reservation/Reservation";
 import styles from "./page.module.scss";
+import MeetUs from "./components/meetUs";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-      <LandingPage />
+      <News />
       <MeetUs />
       <Reservation />
       <Menu />
