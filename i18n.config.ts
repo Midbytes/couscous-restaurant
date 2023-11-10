3ad005c70035191d02c4afb25578ea8b88e4fb8d
alibeji98 <alibeji98@gmail.com>
2023-11-10 14:15:54 +0100

    feat: add supported locales and types

---
+++ i18n.config.ts
@@ -0,0 +1,9 @@
+export enum Locales {
+  English = "en",
+  Danish = "da",
+}
+export const i18n = {
+  locales: [Locales.English, Locales.Danish],
+  //TODO: change to "Locales.Danish"
+  defaultLocale: Locales.English,
+};
