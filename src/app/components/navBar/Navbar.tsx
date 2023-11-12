d75d4e9db36cb12dcd237fd761429994c359b1e7
alibeji98 <alibeji98@gmail.com>
2023-11-12 13:39:31 +0100

    refactor: remove logo from navbar

--- src/app/components/navBar/Navbar.tsx
+++ src/app/components/navBar/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./navBar.module.scss";
-import Image from "next/image";
 import { NAV_ITEM } from "@/app/constant/navItem";
 import LinkNavigation from "../link/LinkNavigation";
 import LanguageSelector from "../languageSelector/LanguageSelector";
@@ -10,12 +9,6 @@ function Navbar({ lang }: { lang: Locales }) {
   return (
     <header className={styles.container}>
       <h1 className={styles.logos}>
-        <Image
-          src="/assets/logo.svg"
-          alt="logo of le couscous restaurant"
-          width={0}
-          height={0}
-        />
         <span>Le Couscous</span>
       </h1>
       <nav className={styles.links}>
