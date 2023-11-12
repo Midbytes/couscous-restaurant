cb14e39e2ec1fffe93b57a34940de2dba5cf5831
alibeji98 <alibeji98@gmail.com>
2023-11-12 13:27:30 +0100

    styles: update gallery design

--- src/app/components/gallery/Gallery.tsx
+++ src/app/components/gallery/Gallery.tsx
@@ -4,34 +4,6 @@ import styles from "./gallery.module.scss";
 
 export const Gallery = () => (
   <section className={styles.gallery}>
-    <div className={styles.doubled}>
-      <div className={styles.imageContainer}>
-        <Image
-          src="https://res.cloudinary.com/dhbzxhoyp/image/upload/v1699739776/malaoui_bread_94f1a11e7f.jpg"
-          alt="Maloui bread"
-          fill
-          className={styles.image}
-        />
-      </div>
-      <div className={styles.imageContainer}>
-        <Image
-          src="https://res.cloudinary.com/dhbzxhoyp/image/upload/v1699739775/fatma_finger_4bed776558.avif"
-          alt="Maloui bread"
-          fill
-          className={styles.image}
-        />
-      </div>
-    </div>
-    <div className={`${styles.doubled} ${styles.right}`}>
-      <div className={`${styles.imageContainer} ${styles.left}`}>
-        <Image
-          src="https://res.cloudinary.com/dhbzxhoyp/image/upload/v1699739776/soup_6d9d3ff2dc.jpg"
-          alt="Maloui bread"
-          fill
-          className={styles.image}
-        />
-      </div>
-    </div>
     <div className={`${styles.imageContainer} ${styles.right}`}>
       <Image
         src="https://res.cloudinary.com/dhbzxhoyp/image/upload/v1699739776/tunisian_plate_3ac8da1d0d.jpg"
--- src/app/constant/navItem.ts
+++ src/app/constant/navItem.ts
@@ -9,10 +9,7 @@ export const NAV_ITEM: NavItem[] = [
     label: { en: "About Us", da: "Om Os" },
     href: "#meet",
   },
-  {
-    label: { en: "Book a Table", da: "Reservere et Bord" },
-    href: "#tables",
-  },
+
   {
     label: { en: "Our Menu", da: "Vores Menu" },
     href: "#menu",
