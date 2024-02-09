b9d5d6380b5907bf278835053e4af0d1b0ffbf8e
alibeji98 <alibeji98@gmail.com>
2024-02-09 16:48:09 +0100

    fix: uncomment Reservation component

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -15,8 +15,7 @@ export default function Home({
     <main className={styles.main}>
       <News lang={lang} />
       <MeetUs lang={lang} />
-      {/*       <Reservation />
-       */}
+      <Reservation />
       <Menu lang={lang} />
       <Delicacies lang={lang} />
     </main>
